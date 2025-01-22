4d06814a09855d2877e83ba302a4dea8ba8595db
Anastasiia Tarasiuk <111896564+anastasiiatarasiuk@users.noreply.github.com>
2025-01-22 12:14:15 +0200

    add navigation menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Navigation from "./components/Navigation/Navigation";
 import TalkToUs from "./components/TalkToUs/TalkToUs";
 
 function App() {
   return (
     <>
       <Header />
+      <Navigation />
       <TalkToUs />
       <Footer />
     </>
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,40 @@
+import "./Navigation.scss";
+
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <div className="navigation__content">
+          <img src="/images/logo.webp" alt="logo" />
+          <nav className="navigation__list">
+            <ul className="list">
+              <li className="list__item">
+                <a href="#home">Home</a>
+              </li>
+              <li className="list__item">
+                <a href="#">Services</a>
+              </li>
+              <li className="list__item">
+                <a href="#">Instructional Design</a>
+              </li>
+              <li className="list__item">
+                <a href="#">eLearning Development</a>
+              </li>
+              <li className="list__item">
+                <a href="#">eLearning Price Quote</a>
+              </li>
+              <li className="list__item">
+                <a href="#">eLearning Packages</a>
+              </li>
+              <li className="list__item">
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navigation;
