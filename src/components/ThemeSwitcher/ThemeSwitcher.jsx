b084a1165fc1cd90a2848d96b54fa8fbae232c9f
Anastasiia Tarasiuk <111896564+anastasiiatarasiuk@users.noreply.github.com>
2025-01-27 16:20:09 +0200

    fix ThemeSwitcher

--- src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,12 +1,36 @@
 import { useTheme } from "../ThemeProvider/ThemeProvider";
+import { useEffect } from "react";
+import "./ThemeSwitcher.scss";
 
 const ThemeSwitcher = () => {
   const { theme, toggleTheme } = useTheme();
 
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") || "light";
+    if (savedTheme !== theme) {
+      toggleTheme();
+    }
+  }, [theme, toggleTheme]);
+
+  const handleToggleTheme = () => {
+    toggleTheme();
+    localStorage.setItem("theme", theme === "light" ? "dark" : "light");
+  };
+
   return (
-    <button onClick={toggleTheme}>
-      {theme === "light" ? "DAYMODE" : "NIGHTMODE"}
-    </button>
+    <label className="theme-toggle">
+      <input
+        className="theme-toggle__checkbox"
+        type="checkbox"
+        onChange={handleToggleTheme}
+        checked={theme === "dark"}
+      />
+      <span className="theme-toggle__slider">
+        <span className="theme-toggle__text">
+          {theme === "light" ? "DAYMODE" : "NIGHTMODE"}
+        </span>
+      </span>
+    </label>
   );
 };
 
